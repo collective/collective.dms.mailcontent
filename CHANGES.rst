c19101c3aa52c431ca7094a143f6bd92e3ff176b
Cédric Messiant <cedricmessiant@ecreall.com>
2013-01-07 13:11:43 +0100

    Initial commit

---
+++ CHANGES.rst
@@ -0,0 +1,8 @@
+Changelog
+=========
+
+1.0-dev (unreleased)
+--------------------
+
+- Package created using templer
+  ["Cédric Messiant"]
---
+++ setup.py
@@ -0,0 +1,45 @@
+#! -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(name='collective.dms.mailcontent',
+      version=version,
+      description="Mail content type for document management system",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='"C\xc3\xa9dric Messiant"',
+      author_email='cedricmessiant@ecreall.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['collective', 'collective.dms'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
