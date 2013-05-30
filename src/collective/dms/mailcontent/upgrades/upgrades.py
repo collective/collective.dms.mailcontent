a53cd50accd89de3128073ee214fa2cc01c322dd
Cédric Messiant <cedricmessiant@ecreall.com>
2013-05-30 11:49:04 +0200

    Add upgrade step to migrate reception dates to datetimes

---
+++ src/collective/dms/mailcontent/upgrades/upgrades.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from datetime import date, datetime, time
+
+from plone import api
+
+
+def v2(context):
+    default_time = time(18, 0)
+    catalog = api.portal.get_tool('portal_catalog')
+    brains = catalog.searchResults({'portal_type': 'dmsincomingmail'})
+    for brain in brains:
+        obj = brain.getObject()
+        reception_date = obj.reception_date
+        if isinstance(reception_date, date):
+            obj.reception_date = datetime.combine(reception_date, default_time)
