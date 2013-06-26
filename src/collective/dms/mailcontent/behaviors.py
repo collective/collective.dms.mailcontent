7f22bc3463f96d64da42c7f0b92d24a3ee112603
Cédric Messiant <cedricmessiant@ecreall.com>
2013-06-26 17:09:11 +0200

    Simplify sending types behavior and vocabulary

--- src/collective/dms/mailcontent/behaviors.py
+++ src/collective/dms/mailcontent/behaviors.py
@@ -1,26 +1,28 @@
 from z3c.form.browser.radio import RadioFieldWidget
 from zope.interface import alsoProvides
 from zope.schema import Choice
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.autoform import directives as form
-from plone.directives.form import default_value
 from plone.supermodel import model
 
 from collective.dms.mailcontent import _
 
 
+sending_types = SimpleVocabulary([
+    SimpleTerm(value='normal', title=_(u'Normal')),
+    SimpleTerm(value='registered', title=_(u'Registered')),
+])
+
+
 class ISendingType(model.Schema):
     """Sending type behavior"""
     sending_type = Choice(title=_(u"Sending type"),
-                          vocabulary='SendingTypes',
+                          vocabulary=sending_types,
+                          default='normal',
                           required=False)
     form.widget(sending_type=RadioFieldWidget)
-
-
-@default_value(field=ISendingType['sending_type'])
-def sending_type_default_value(data):
-    return "normal"
 
 
 alsoProvides(ISendingType, IFormFieldProvider)
