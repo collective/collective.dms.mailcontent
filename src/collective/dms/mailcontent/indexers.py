b5365b7d70e2ca668fe1024b19efe9f057e06226
Stephan Geulette <s.geulette@imio.be>
2016-06-22 14:46:50 +0200

    Use list and not set to avoid set stored in column attribute

--- src/collective/dms/mailcontent/indexers.py
+++ src/collective/dms/mailcontent/indexers.py
@@ -7,7 +7,7 @@ from collective.dms.basecontent.dmsdocument import IDmsDocument
 
 def add_parent_organizations(obj, index):
     for org in obj.get_organizations_chain():
-        index.add('l:%s' % org.UID())
+        index.append('l:%s' % org.UID())
 
 
 @indexer(IDmsDocument)
@@ -20,7 +20,7 @@ def sender_index(obj):
     # we check the stored value that must contain a z3c.relationfield.relation.RelationValue object
     if not obj.sender or obj.sender.isBroken():
         return _marker
-    index = set([obj.sender.to_object.UID()])
+    index = [obj.sender.to_object.UID()]
 
     if obj.sender.to_object.portal_type == 'organization':
         add_parent_organizations(obj.sender.to_object, index)
@@ -38,18 +38,19 @@ def recipients_index(obj):
     """
     if not obj.recipients:
         return _marker
-    index = set()
+    index = []
 
     for rel in obj.recipients:
         if rel.isBroken():
             continue
         recip = rel.to_object
-        index.add(recip.UID())
+        index.append(recip.UID())
 
         if recip.portal_type == 'organization':
             add_parent_organizations(recip, index)
         elif recip.portal_type == 'held_position':
             add_parent_organizations(recip.get_organization(), index)
     if index:
-        return index
+        # make unique items
+        return list(set(index))
     return _marker
