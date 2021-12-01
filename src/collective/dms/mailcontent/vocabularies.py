15feab895c297bb5e710e283c2d9043f643a09ab
Stephan Geulette <s.geulette@imio.be>
2021-12-01 15:38:28 +0100

    Added in EmailAttachmentsVocabulary all related files

--- src/collective/dms/mailcontent/vocabularies.py
+++ src/collective/dms/mailcontent/vocabularies.py
@@ -2,7 +2,8 @@
 """Vocabularies."""
 from collective.dms.basecontent.dmsfile import IDmsAppendixFile
 from collective.dms.basecontent.dmsfile import IDmsFile
-from collective.dms.mailcontent import _ as _no  # noqa
+from collective.dms.mailcontent import _tr
+from imio.helpers.content import find
 from plone import api
 from Products.CMFPlone.utils import safe_unicode
 from zope.interface import implements
@@ -17,10 +18,22 @@ class EmailAttachmentsVocabulary(object):
 
     def __call__(self, context):
         terms = []
-        brains = api.content.find(context=context, object_provides=[IDmsAppendixFile.__identifier__,
-                                                                    IDmsFile.__identifier__])
+        # first we find context files, unrestrictedly
+        brains = find(context=context, unrestricted=True, object_provides=[IDmsAppendixFile.__identifier__,
+                                                                           IDmsFile.__identifier__])
         for brain in brains:
-            terms.append(SimpleTerm(brain.UID, brain.UID, _no(u'{}: ${{title}}'.format(brain.portal_type),
+            terms.append(SimpleTerm(brain.UID, brain.UID, _tr(u'({}) => ${{title}}'.format(brain.portal_type),
                                                               mapping={'title': safe_unicode(brain.Title)})))
+        # then we find files of related mails
+        pc = api.portal.get_tool('portal_catalog')
+        for rv in context.reply_to:
+            brains = pc.unrestrictedSearchResults(path=rv.to_path, object_provides=[IDmsAppendixFile.__identifier__,
+                                                                                    IDmsFile.__identifier__])
+            for brain in brains:
+                terms.append(SimpleTerm(brain.UID, brain.UID,
+                                        _tr(u'${{ref}} ({}) => ${{title}}'.format(brain.portal_type),
+                                            mapping={'ref': rv.to_object.internal_reference_no,
+                                                     'title': safe_unicode(brain.Title)})))
+
         terms.sort(key=lambda trm: trm.title.lower())
         return SimpleVocabulary(terms)
