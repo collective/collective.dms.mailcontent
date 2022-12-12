9504b2ff505f46456853e2a6e48635bee71d75a2
Stephan Geulette <s.geulette@imio.be>
2022-12-12 10:49:20 +0100

    Preparing release 1.12

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.12 (unreleased)
+1.12 (2022-12-12)
 -----------------
 
 - Added intermediate commit option in v11 upgrade
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = '1.12.dev0'
+version = '1.12'
 
 long_description = (
     open('README.rst').read()
