0a08729291e93277c5e623bddaccf5e79cefa76d
Stephan Geulette <s.geulette@imio.be>
2024-09-26 12:00:51 +0200

    Back to development: 1.16.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "1.16.2"
+version = "1.16.3.dev0"
 
 long_description = (
     open("README.rst").read() + "\n" + "Contributors\n"
